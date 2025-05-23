4ec8dc11e855637c5028816a6c994401b2ee4bd0
Jorge Gutierrez <milo07@gmail.com>
2025-05-23 11:48:49 -0400

    v3-backend

--- server.js
+++ server.js
@@ -9,17 +9,22 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/api/reviews", async (req, res) => {
-  try {
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${process.env.PLACE_ID}&fields=name,rating,reviews&key=${process.env.GOOGLE_API_KEY}`;
-    const response = await axios.get(url);
-    const reviews = response.data.result.reviews || [];
-    res.json(reviews);
-  } catch (err) {
-    console.error("Error fetching reviews:", err);
-    res.status(500).json({ error: "Error retrieving reviews" });
-  }
+    try {
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${process.env.PLACE_ID}&fields=name,rating,reviews&key=${process.env.GOOGLE_API_KEY}`;
+        const response = await axios.get(url);
+        const reviews = response.data.result.reviews || [];
+        res.json(reviews);
+    } catch (err) {
+        console.error("Error fetching reviews:", err);
+        res.status(500).json({ error: "Error retrieving reviews" });
+    }
 });
 
+app.get("/", (req, res) => {
+    res.send("✅ La API de reseñas está online");
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
